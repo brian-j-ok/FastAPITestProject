7021fc14b2261886b89c783a9fc6bf501f0f915e
Brian O'Kelly <brianjrok@gmail.com>
2022-11-06 17:41:07 -0700

    coded in logic to create mock User db and get request for specific user

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
 from fastapi import FastAPI
+from models import User, Role
+from typing import List
+from uuid import UUID, uuid4
+
+import json
 
 app = FastAPI()
 
+db: List[User] = []
 
+def fetch_users():
+    file = open('MOCK_USER_DATA.json')
+    data = json.load(file)
+
+    for person in data:
+        user = User(
+            id=uuid4(),
+            first_name=person['first_name'],
+            last_name=person['last_name'],
+            email=person['email'],
+            roles=[Role.user]
+        )
+        db.append(user)
+
+
+fetch_users()
+
+# Path operations are evaluated in order!!!
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/users")
+async def get_users():
+    return db
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: UUID):
+    for user in db:
+        return user if user.id == user_id else {"message": "User not found..."}
